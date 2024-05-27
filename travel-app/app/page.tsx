016cd6521afdb31e91717cfae506d6a92e596f31
Jordyn Jensen <jordyneajensen@gmail.com>
2024-05-27 13:28:03 -0500

    finished camp section

--- travel-app/app/page.tsx
+++ travel-app/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
+import './globals.css';
 
 export default function Home() {
   return (
